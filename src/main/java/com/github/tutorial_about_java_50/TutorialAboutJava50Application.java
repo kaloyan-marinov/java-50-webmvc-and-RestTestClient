9a98fa871b0d1030daaa62625e492dad5f4c7077
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-07-19 11:07:27 +0200

    initialize this as Spring Boot project by (starting from the parent directory and issuing the following commands): ```shell $ curl https://start.spring.io/starter.zip \     -d type=maven-project \     -d language=java \     -d bootVersion=4.1.0 \     -d groupId=com.github \     -d artifactId=tutorial-about-java-50 \     -d packageName=com.github.tutorial-about-java-50 \     -d packaging=jar \     -d javaVersion=25 \     -d dependencies=web,spring-restclient \     -o tutorial-about-java-50.zip
    $ unzip tutorial-about-java-50.zip \
        -d tutorial-about-java-50
    
    $ rm tutorial-about-java-50.zip
    ```

---
+++ src/main/java/com/github/tutorial_about_java_50/TutorialAboutJava50Application.java
@@ -0,0 +1,13 @@
+package com.github.tutorial_about_java_50;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialAboutJava50Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialAboutJava50Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tutorial_about_java_50/TutorialAboutJava50ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tutorial_about_java_50;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialAboutJava50ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
